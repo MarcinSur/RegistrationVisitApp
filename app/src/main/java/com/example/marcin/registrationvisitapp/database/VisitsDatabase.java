c21717418b919f83605eaa59429ef48a92ba2375
Marcin <marcin@great.com.pl>
2018-06-05 12:43:29 +0200

    Add @TypeConverters to Date

--- app/src/main/java/com/example/marcin/registrationvisitapp/database/VisitsDatabase.java
+++ app/src/main/java/com/example/marcin/registrationvisitapp/database/VisitsDatabase.java
@@ -3,12 +3,15 @@ package com.example.marcin.registrationvisitapp.database;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
 import android.content.Context;
 
+import com.example.marcin.registrationvisitapp.converter.DateConverter;
 import com.example.marcin.registrationvisitapp.data.Visit;
 import com.example.marcin.registrationvisitapp.data.VisitDao;
 
 @Database(entities = {Visit.class}, version = 1)
+@TypeConverters(DateConverter.class)
 public abstract class VisitsDatabase extends RoomDatabase {
 
     private static VisitsDatabase INSTANCE;
@@ -19,7 +22,7 @@ public abstract class VisitsDatabase extends RoomDatabase {
                     INSTANCE = Room.databaseBuilder(
                             context.getApplicationContext(),
                             VisitsDatabase.class,
-                            "visit_dayabase")
+                            "visit_database")
                             .build();
                 }
             }
